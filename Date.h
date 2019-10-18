08b0e6ed7307bbe79ad86b3e3d091c72550d5c19
Davide Castro <davideafc@gmail.com>
2019-10-18 16:28:31 +0100

    Added Date.cpp and Date.h

---
+++ Date.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+#include "defs.h"
+
+using namespace std;
+
+class Date {
+private:
+	unsigned short day;
+	unsigned short month;
+	unsigned year;
+
+public:
+	// Constructors
+
+	Date(); // Default constructor
+	Date(string date); // constructor from a string date
+	Date(unsigned short day, unsigned short month, unsigned year); // constructor from the separate elements of the date
+
+	// GET methods
+	unsigned short getDay() const;
+	unsigned short getMonth() const;
+	unsigned getYear() const;
+	string getDate() const; // get the date on the form of a string (year/month/day)
+
+	// SET methods
+
+	void setDay(unsigned short day);
+	void setMonth(unsigned short month);
+	void setYear(unsigned year);
+
+	// other methods
+
+	friend ostream& operator<<(ostream& out, const Date & date); // overload of operator << (display the formatted date on a ostream)
+
+};
