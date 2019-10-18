8f748a3aa6fbe4e8cb75d845da5d55d4d2d8a9f5
Davide Castro <davideafc@gmail.com>
2019-10-18 21:57:42 +0100

    Added Time.h and Time.cpp
    Implemented class Time and some of its methods

---
+++ AEDA---Airports/Time.cpp
@@ -0,0 +1,29 @@
+#include "Utils.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Time.h"
+
+using namespace std;
+
+Time::Time() {}
+
+Time::Time(unsigned int hours, unsigned int minutes)
+{
+	this->hours = hours;
+	this->minutes = minutes;
+}
+
+unsigned int Time::getHours() const { return hours; }
+
+unsigned int Time::getMinutes() const { return minutes; }
+
+bool Time::isValid() const 
+{
+	if (hours <= 23 && hours >= 0 && minutes <= 59 && minutes >= 0)
+		return true;
+	else 
+		return false;
+}
---
+++ AEDA---Airports/Time.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Utils.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+class Time
+{
+private:
+	unsigned int hours, minutes;
+
+public:
+	Time();
+	Time(unsigned int hours, unsigned int minutes);
+	unsigned int getHours() const;
+	unsigned int getMinutes() const;
+	bool isValid() const;
+	bool operator<(Time &time) const;
+};
