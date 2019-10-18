95439f98884a3ea96c868739ed57e58f5ddbed56
Diogo <diogogrosario1008@gmail.com>
2019-10-18 16:18:40 +0100

    Added Utils.h and Utils.cpp, added bool isValid and operator < on date C

---
+++ AEDA---Airports/Utils.cpp
@@ -0,0 +1,19 @@
+#include "Utils.h"
+
+bool leapYear(int year)
+{
+	if (year % 4 == 0 && year % 100 != 0)
+		return true;
+	else if (year % 4 == 0 && year % 400 == 0)
+		return true;
+	return false;
+}
+
+int days(int year, int month)
+{
+	int d[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	if (month == 2 && leapYear(year))
+		return 29;
+	else
+		return d[month - 1];
+}
---
+++ AEDA---Airports/Utils.h
@@ -0,0 +1,5 @@
+#pragma once
+
+bool leapYear(int year);
+
+int days(int year, int month);
