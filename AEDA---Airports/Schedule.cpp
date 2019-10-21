1a4a95a172334cdc99c035fb630b99cd0da6d936
Davide Castro <davideafc@gmail.com>
2019-10-21 23:31:21 +0100

    Added Schedule.h and Schedule.cpp
    Implemented class Schedule and FlightSched

---
+++ AEDA---Airports/Schedule.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Utils.h"
+#include "Schedule.h"
+#include "Time.h"
+#include "Date.h"
+
+using namespace std;
+
+Schedule::Schedule() {}
+
+Schedule::Schedule(Time &start, Time &end)
+{
+	this->start = start;
+	this->end = end;
+}
+
+Time Schedule::getStart() const { return start; }
+
+Time Schedule::getEnd() const { return end; }
+
+Time Schedule::getDuration() const
+{
+	Time duration;
+	duration.setHours(end.getHours() - start.getHours());
+	duration.setMinutes(end.getMinutes() - start.getMinutes());
+	return duration;
+}
+
+Time Schedule::setStart(Time &start)
+{
+	this->start = start;
+	return start;
+}
+
+Time Schedule::setEnd(Time &end)
+{
+	this->end = end;
+}
+
+
+
+/*---------------FLIGHT SCHEDULE-------------*/
+
+FlightSched::FlightSched() {}
+
+FlightSched::FlightSched(Date departureD, Time departureH, Date arrivalD, Time arrivalH)
+{
+	departureDate = departureD;
+	departureHour = departureH;
+	arrivalDate = arrivalD;
+	arrivalHour = arrivalH;
+}
+
+Date FlightSched::getDepartureDate() const
+{
+	return Date();
+}
+
+Date FlightSched::getArrivalDate() const
+{
+	return Date();
+}
+
+Time FlightSched::getDepartureHour() const
+{
+	return Time();
+}
+
+Time FlightSched::getArrivalHour() const
+{
+	return Time();
+}
+
+bool FlightSched::isValid() const
+{
+	Date departureD = departureDate;
+	Date arrivalD = arrivalDate;
+	Time departureH = departureHour;
+	Time arrivalH = arrivalHour;
+	if (departureD.isValid() && arrivalD.isValid() && departureH.isValid() && arrivalH.isValid())
+	{
+		if (departureD == arrivalD) //por alguma razao nao me deixa igualar departureDate e arrivalDate
+		{
+			if (departureH < arrivalH)
+				return true;
+		}
+		else if (departureD < arrivalD)
+			return true;
+		else return false;
+	}
+	else return false;
+}
+
---
+++ AEDA---Airports/Schedule.h
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Utils.h"
+#include "Time.h"
+#include "Date.h"
+
+
+class Schedule
+{
+private:
+	Time start;
+	Time end;
+
+public:
+	Schedule();
+	Schedule(Time &start, Time &end);
+	Time getStart() const;
+	Time getEnd() const;
+	Time getDuration() const;
+	Time setStart(Time &start);
+	Time setEnd(Time &end);
+};
+
+class FlightSched
+{
+private:
+	Date departureDate;
+	Date arrivalDate;
+	Time departureHour;
+	Time arrivalHour;
+public:
+	FlightSched();
+	FlightSched(Date departureD, Time departureH, Date arrivalD, Time arrivalH);
+	Date getDepartureDate() const;
+	Date getArrivalDate() const;
+	Time getDepartureHour() const;
+	Time getArrivalHour() const;
+
+	bool isValid() const;
+};
