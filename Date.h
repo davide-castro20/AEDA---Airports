22400c338a64772fd5ecccf2d8c30d7fb703799c
Henrique <gamingwithapotato@gmail.com>
2019-11-15 20:06:04 +0000

    Added comments

--- Date.h
+++ Date.h
@@ -18,22 +18,55 @@ public:
 	Date(unsigned short day, unsigned short month, unsigned year); // constructor from the separate elements of the date
 
 	// GET methods
+	/**
+@brief Returns the day corresponding to the selecated "Date"
+*/
 	unsigned short getDay() const;
+	/**
+@brief Returns the month corresponding to the selecated "Date"
+*/
 	unsigned short getMonth() const;
+	/**
+@brief Returns the year corresponding to the selecated "Date"
+*/
 	unsigned getYear() const;
+	/**
+@brief Returns the string of the date corresponding to the selecated "Date"
+*/
 	string getDate() const; // get the date on the form of a string (year/month/day)
 
 	// SET methods
 
+	/**
+@brief Sets the day in the selecated "Date"
+*/
 	void setDay(unsigned short day);
+	/**
+@brief Sets the month in the selecated "Date"
+*/
 	void setMonth(unsigned short month);
+	/**
+@brief Sets the day in the selecated "Date"
+*/
 	void setYear(unsigned year);
 
 	// other methods
 
+	/**
+@brief Shows the "Date" in a user friendly way
+*/
 	friend ostream& operator<<(ostream& out, const Date & date); // overload of operator << (display the formatted date on a ostream)
+	/**
+@brief Compares if the date is smaller than the one recived in the parameter
+*/
 	bool operator<(const Date &date);
+	/**
+@brief Compares if the date is smaller or equal than the one recived in the parameter
+*/
 	bool operator<=(const Date &date);
+	/**
+@brief Compares if the date is equal to the one recived in the parameter
+*/
 	bool operator==(const Date &date);
 	/**
 @brief Checks if the date is valid
--- Time.h
+++ Time.h
@@ -12,18 +12,48 @@ private:
 public:
 	Time();
 	Time(unsigned int hours, unsigned int minutes);
+	/**
+@brief Returns the hours of the "Time" object
+*/
 	unsigned int getHours() const;
+	/**
+@brief Returns the minutes of the "Time" object
+*/
 	unsigned int getMinutes() const;
+	/**
+@brief Returns the total minutes of the "Time" object
+*/
 	unsigned int getTotalMinutes() const;
+	/**
+@brief Sets the hours of the "Time" object
+*/
 	void setHours(unsigned int hours);
+	/**
+@brief Sets the minutes of the "Time" object
+*/
 	void setMinutes(unsigned int hours);
 	/**
 @brief Checks if time is valid
 */
 	bool isValid() const;
+	/**
+@brief Compares if the time is before than the one recived in the parameter
+*/
 	bool operator<(const Time &time) const;
+	/**
+@brief Compares if the ´time is equal to the one recived in the parameter
+*/
 	bool operator==(const Time &time) const;
+	/**
+@brief Returns the diference between the time and the one received in the parameter
+*/
 	Time operator-(Time &time) const;
+	/**
+@brief Shows the time in a user friendly way
+*/
 	friend ostream& operator<<(ostream& out, const Time& time);
+	/**
+@brief Returns a string with a formated time
+*/
 	string getTime() const;
 };
